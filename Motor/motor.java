9ecab72c90329caac60649af4f3186159e81a0dc
Vitor Franca <vsfranca@sga.pucminas.br>
2019-03-31 10:22:18 -0300

    Criacao motor

---
+++ Motor/motor.java
@@ -0,0 +1,13 @@
+import org.gnu.glpk.GLPK;
+import org.gnu.glpk.GLPKConstants;
+import org.gnu.glpk.GlpkException;
+import org.gnu.glpk.SWIGTYPE_p_double;
+import org.gnu.glpk.SWIGTYPE_p_int;
+import org.gnu.glpk.glp_prob;
+import org.gnu.glpk.glp_smcp;
+
+public class motor {
+    public static void main(String[] args){
+
+    }
+}
