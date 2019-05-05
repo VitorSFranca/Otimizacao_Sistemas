1512f70aa518a2ad4ea51999c22b90ceffd8e571
Vitor Franca <vsfranca@sga.pucminas.br>
2019-05-04 22:55:13 -0300

    Variaveis no modelo GLPK

--- OS_API/OS_API/Services/GlpkService.cs
+++ OS_API/OS_API/Services/GlpkService.cs
@@ -21,7 +21,25 @@ namespace OS_API.Services
             {
                 var model = new Model();
 
-                var variables = new Variable[20];
+                #region Variaveis
+                var variables = new Variable[9];
+                variables[0] = new Variable("boloCenouraCalda", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloCenoura", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloChocolateCalda", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloChocolate", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloMexerica", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloLaranja", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloFuba", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloLimao", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                variables[0] = new Variable("boloBanana", 0, System.Double.PositiveInfinity, VariableType.Integer);
+                #endregion
+
+                // Aqui devemos colocar a funcao objetivo:
+                // x[i]*(CustoDoBoloI+LucroDoBoloI) + x[i+2]*(CustoDoBoloI2+LucroDoBoloI2) ...
+                // Deve ser feito um calculo dos custos dos ingredientes que cada bolo utiliza para ser subtraido pela quantidade de bolos
+                // @TODO Metodo para calcular o custo de cada bolo bem como ter os lucros de cada bolo aqui para saber o lucro por unidade
+                model.AddObjective(x[0] * , string.Empty, ObjectiveSense.Maximize);
+
             }
 
             return entity.qtdOvos.ToString();
